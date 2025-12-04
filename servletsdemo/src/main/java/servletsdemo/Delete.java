5070310ada27e9a255606cbc4b46d65f3d7cee70
ashishmishra3648 <am8419941@gmail.com>
2025-12-04 22:14:26 +0530

    Update Delete.java

--- servletsdemo/src/main/java/servletsdemo/Delete.java
+++ servletsdemo/src/main/java/servletsdemo/Delete.java
@@ -13,15 +13,25 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Delete extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int id = Integer.parseInt(req.getParameter("id"));
-		Student student = new Student(id);
-		Service service = new Service();
-		int res = service.delete(student);
-		PrintWriter pr = resp.getWriter();
-		if (res > 0) {
-			pr.write("<h1>DELETE Successfully</h1>");
-		} else {
-			pr.write("<h1>DELETE Failed</h1>");
+		try {
+			int id = Integer.parseInt(req.getParameter("id"));
+			Student student = new Student(id);
+			Service service = new Service();
+			int res = service.delete(student);
+			PrintWriter pr = resp.getWriter();
+			resp.setContentType("text/html");
+			if (res > 0) {
+				pr.write("<h1>DELETE Successfully</h1>");
+				pr.write("<a href='Home.jsp'>Go Home</a>");
+			} else {
+				pr.write("<h1>DELETE Failed</h1>");
+				pr.write("<a href='Delete.jsp'>Try Again</a>");
+			}
+		} catch (NumberFormatException e) {
+			PrintWriter pr = resp.getWriter();
+			resp.setContentType("text/html");
+			pr.write("<h1>Invalid Input</h1>");
+			pr.write("<a href='Delete.jsp'>Try Again</a>");
 		}
 	}
 }
